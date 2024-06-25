770d15085d1ac221724f99ee194217ecbbbded74
Pier Fumagalli <pier@usrz.com>
2024-06-25 18:15:04 +0200

    Export font locations

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export { FNC1, FNC2, FNC3, FNC4, code128 } from './code128'
 export { checksum, ean } from './ean'
+
+const here = new URL(__fileurl, 'file:///')
+
+export const fonts = {
+  'LibreBarcode128-Regular.ttf': new URL('../assets/LibreBarcode128-Regular.ttf', here).pathname,
+  'LibreBarcode128Text-Regular.ttf': new URL('../assets/LibreBarcode128Text-Regular.ttf', here).pathname,
+  'LibreBarcodeEAN13Text-Regular.ttf': new URL('../assets/LibreBarcodeEAN13Text-Regular.ttf', here).pathname,
+}
